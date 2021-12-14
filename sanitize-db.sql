8a40b7f585eb0efbedb61d0d90e34630b9ecfe3a
Neven Kajić <nkajic@users.noreply.github.com>
2021-12-14 14:42:37 +0100

    Create sanitize-db.sql

---
+++ sanitize-db.sql
@@ -0,0 +1,17 @@
+; Sanitize existing db from production
+
+; Delete customer tables
+DELETE FROM customer_entity;
+DELETE FROM customer_grid_flat;
+
+; Delete sales order tables
+DELETE FROM sales_order;
+DELETE FROM sales_order_grid;
+
+; Delete invoice tables
+DELETE FROM sales_invoice;
+DELETE FROM sales_invoice_grid;
+
+; Delete credit memo tables
+DELETE FROM sales_creditmemo;
+DELETE FROM sales_creditmemo_grid;
