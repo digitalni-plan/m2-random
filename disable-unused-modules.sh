f302f583349e9d7ab8a1c10f32bc685fa5613481
Neven Kajić <nkajic@users.noreply.github.com>
2022-01-22 17:15:17 +0100

    Update disable-unused-modules.sh

--- disable-unused-modules.sh
+++ disable-unused-modules.sh
@@ -1,1 +1,35 @@
-
+bin/magento module:disable \
+Magento_Paypal \
+Magento_PaypalGraphQl \
+Magento_ReCaptchaPaypal \
+Magento_PaypalCaptcha \
+PayPal_Braintree \
+PayPal_BraintreeGraphQl \
+Amazon_Core \
+Amazon_Login \
+Amazon_Payment \
+Temando_ShippingRemover \
+Yotpo_Yotpo \
+Magento_AdobeIms \
+Magento_AdobeImsApi \
+Magento_AdobeStockAdminUi \
+Magento_AdobeStockAssetApi \
+Magento_AdobeStockClient \
+Magento_AdobeStockClientApi \
+Magento_AdobeStockImage \
+Magento_AdobeStockImageApi \
+Magento_AdobeStockAsset \
+Magento_AdobeStockImageAdminUi \
+Klarna_Core \
+Klarna_Ordermanagement \
+Klarna_Kp \
+Klarna_Onsitemessaging \
+Klarna_KpGraphQl \
+Magento_AwsS3 \
+Magento_AwsS3PageBuilder \
+Magento_CatalogPageBuilderAnalytics \
+Magento_CmsPageBuilderAnalytics \
+Magento_PageBuilder \
+Magento_PageBuilderAnalytics \
+Magento_PageBuilderAdminAnalytics \
+Magento_AwsS3PageBuilder
